b7bec66fa02c676ba2c802378694498151cb15dd
Hugo <hugojoly2803@gmail.com>
2024-02-26 09:50:36 +0100

    slider et cases

--- tp2/lib/exo5/mainexo5B.dart
+++ tp2/lib/exo5/mainexo5B.dart
@@ -8,15 +8,15 @@ class Tile {
 
   Tile({required this.imageURL, required this.alignment});
 
-  Widget croppedImageTile() {
+  Widget croppedImageTile(double tileSize) {
     return FittedBox(
       fit: BoxFit.fill,
       child: ClipRect(
         child: Container(
           child: Align(
             alignment: alignment,
-            widthFactor: 1/3,
-            heightFactor: 1/3,
+            widthFactor: 1 / tileSize,
+            heightFactor: 1 / tileSize,
             child: Image.network(this.imageURL),
           ),
         ),
@@ -37,8 +37,14 @@ List<Tile> tiles = [
   Tile(imageURL: 'https://picsum.photos/512', alignment: Alignment(1, 1))
 ];
 
-class Exo5B extends StatelessWidget {
-int _currentSliderSize = 1;
+class Exo5B extends StatefulWidget {
+  @override
+  _Exo5BState createState() => _Exo5BState();
+}
+
+class _Exo5BState extends State<Exo5B> {
+  double gridSize = 3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,35 +53,47 @@ int _currentSliderSize = 1;
         centerTitle: true,
       ),
       body: Column(
-          children: [
-            GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: _currentSliderSize,
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-          childAspectRatio: 1.0,
-        ),
-        itemCount: tiles.length,
-        itemBuilder: (BuildContext context, int index) {
-          return createTileWidgetFrom(tiles[index]);
-        },
+        children: [
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: gridSize.toInt(),
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: tiles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return createTileWidgetFrom(tiles[index], gridSize);
+              },
+            ),
+          ),
+          Slider(
+            value: gridSize,
+            min: 1,
+            max: 10,
+            divisions: 9,
+            onChanged: (value) {
+              setState(() {
+                gridSize = value;
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              gridSize.toInt().toString(),
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
       ),
-    _SliderSize(
-          value: _currentSliderSize,
-          onChanged: (double value) {
-            setState(() {
-              _currentSliderSize = value;
-            });
-          },
-          label: 'Size :',
-        ),
-      ],
-    ));
+    );
   }
 
-  Widget createTileWidgetFrom(Tile tile) {
+  Widget createTileWidgetFrom(Tile tile, double size) {
     return InkWell(
-      child: tile.croppedImageTile(),
+      child: tile.croppedImageTile(size),
       onTap: () {
         print("tapped on tile");
       },
